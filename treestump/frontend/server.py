5ba5647cb4cc148a23278e39fbfa82fbce2ee53e
Neha Narula <narula@gmail.com>
2012-03-03 21:01:01 -0500

    ugh

--- treestump/frontend/server.py
+++ treestump/frontend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, request
 app = Flask(__name__)
 import tweets
 
@@ -14,5 +14,18 @@ def hello():
     out+="</body></html>"
     return out
 
+@app.route("/location")
+def show_tweets():
+    lat = float(request.args['lat'])
+    long = float(request.args['long'])
+    out = "<html><body>"
+    twits,popular = tweets.good_tweets(tweets.popular_hashtags(lat, long))
+    for t in twits:
+        out += "User: %s %s <br>" % (t.user.screen_name, t.text)
+    for ht in popular:
+        out += "Hashtag: %s Count: %d<br>" % (ht[0], ht[1])
+    out+="</body></html>"
+    return out
+    
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
