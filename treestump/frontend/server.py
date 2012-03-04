a44c4dab6446e5bc6ee41944dbd3dee86980edfe
Neha Narula <narula@gmail.com>
2012-03-03 21:05:13 -0500

    venues

--- treestump/frontend/server.py
+++ treestump/frontend/server.py
@@ -1,18 +1,12 @@
 from flask import Flask, url_for, request
-app = Flask(__name__)
 import tweets
+import foursquare
 
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    out = "<html><body>"
-    twits,popular = tweets.run()
-    for t in twits:
-        out += "User: %s %s <br>" % (t.user.screen_name, t.text)
-    for ht in popular:
-        out += "Hashtag: %s Count: %d<br>" % (ht[0], ht[1])
-    out+="</body></html>"
-    return out
+    return "Hello World!"
 
 @app.route("/tweets")
 def show_tweets():
@@ -24,6 +18,18 @@ def show_tweets():
         out += "User: %s %s <br>" % (t.user.screen_name, t.text)
     for ht in popular:
         out += "Hashtag: %s Count: %d<br>" % (ht[0], ht[1])
+    out+="</body></html>"
+    return out
+
+@app.route("/venues")
+def show_venues():
+    lat = float(request.args['lat'])
+    long = float(request.args['long'])
+    out = "<html><body>"
+    fs = foursquare.Foursquare()
+    venues = fs.venues(lat, long, 10)
+    for v in venues:
+        out += "Name: %s Location: %s <br>" % (v.name, v.location)
     out+="</body></html>"
     return out
     
