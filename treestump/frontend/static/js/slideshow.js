d077aed7dc1c2d11d1ca2ffe5a3bd41e48245fc9
Lydia Gu <zhenya.gu@gmail.com>
2012-03-04 12:31:27 -0500

    updated slideshow to loop through data and slide to new divs

--- treestump/frontend/static/js/slideshow.js
+++ treestump/frontend/static/js/slideshow.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
 
         $('.image-view').css("left", 0);
-        $('.image-view2').css("left", -$('.image-view2').outerWidth());
+        $('.text-view').css("left", -$('.image-view').outerWidth());
+        $('.text-view').addClass('current');
+        $('.image-view').addClass('future');
 
         var date = new Date(2012, 3, 4);
-        var data = ["image", 42.363539, -71, date, "Tom", "@tom", "what's up", "http://distilleryimage7.instagram.com/3e917a22654b11e180c9123138016265_7.jpg"];
+        var data = [["image", 42.363539, -71, date, "Tom", "@tom", "what's up", "http://distilleryimage7.instagram.com/3e917a22654b11e180c9123138016265_7.jpg"],
+["image", 42.363539, -71, date, "Tom", "@tom", "what's up", "http://distilleryimage7.instagram.com/3e917a22654b11e180c9123138016265_7.jpg"],
+                    ["text", 42.363539, -71, date, "Tom", "@tom", "what's up", "http://distilleryimage7.instagram.com/3e917a22654b11e180c9123138016265_7.jpg"],
+["image", 42.363539, -71, date, "Tom", "@tom", "what's up", "http://distilleryimage7.instagram.com/3e917a22654b11e180c9123138016265_7.jpg"],
+                    ["text", 42.363539, -71, date, "Tom", "@tom", "what's up", "http://distilleryimage7.instagram.com/3e917a22654b11e180c9123138016265_7.jpg"]];
 
         // change view depending on data
-        set_view(data);
+        var i =0;
+        setTimeout(update,3000);
+
+        function update(){
+            set_view(data[i]);
+            i += 1;
+            if (i < data.length) {
+                setTimeout(update, 3000);
+            }
+        }
 
     });
 
@@ -15,34 +30,33 @@ function set_view(data) {
 
     var type = data[0];
 
+    // remove current and set future to current
+    $('.current').addClass('previous');
+    $('.previous').remove();
+    $('.future').addClass('current');
+    $('.current').removeClass('future');
+
     if (type == "image") {
-
-        if (parseInt($('.image-view').css("left")) < 0) {
-            $('.image-view').css("left", $('.image-view').outerWidth());
-
-        $('.image-view2').animate({
-                left: -$('.image-view').outerWidth()
-                    }, 1000, function() {
-                setTimeout(function(){set_view(data)},3000);
-            });
-        $('.image-view').animate({
-                left: 0
-                    }, 1000, function() {
-            });
-
-        } else if (parseInt($('.image-view2').css("left")) < 0) {
-            $('.image-view2').css("left", $('.image-view2').outerWidth());
-
-            $('.image-view').animate({
-                left: -$('.image-view').outerWidth()
-                    }, 1000, function() {
-            });
-            $('.image-view2').animate({
-                left: 0
-                    }, 1000, function() {
-                    setTimeout(function(){set_view(data)},3000);
-            });
-
-        }
+        // add new div and set left to edge of screen
+        $("body").append('<div class="image-view panel future">'+
+                         '<div class="image">'+
+                         '<img src="'+data[7]+'".>'+
+                         '</div></div>');
+    } else if (type == "text") {
+        // add new div and set left to edge of screen
+        $("body").append('<div class="text-view panel future">'+
+                                '<div class="tweet">'+
+                                '<div>'+data[6]+'</div>'+
+                         '</div></div>');
     }
+    $(".future").css("left",$('.future').outerWidth());
+
+    $('.current').animate({
+            left: -$('.image-view').outerWidth()
+                }, 1000, function() {
+        });
+    $('.future').animate({
+            left: 0
+                }, 1000, function() {
+        });
 }
